7bfa3ae920d606797cb04e4e7fd0a89a22a88a95
N-Le-1 <156348689+N-Le-1@users.noreply.github.com>
2024-01-17 16:22:33 +0000

    upload insertionSort

---
+++ lab2/insertionSort.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void insertionSort(int numArray[], int num) {
+    int i, key, j;
+    for (i = 1; i < num; i++) {
+        key = numArray[i];
+        j = i - 1;
+
+        while (j >= 0 && numArray[j] > key) {
+            numArray[j + 1] = numArray[j];
+            j = j - 1;
+        }
+        numArray[j + 1] = key;
+    }
+}
+
+int main() {
+    int arr[10];
+    int n = 10;
+
+    printf("Enter 10 integers: (Please include spaces in between the integers.):\n");
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    insertionSort(arr, n);
+
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
